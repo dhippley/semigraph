f81978a66145e1f52155c3b821329f76ae695883
Daniel Hippley <1974398+dhippley@users.noreply.github.com>
2025-07-23 14:11:29 -0400

    adds Nx dep

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule Semigraph.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      # Matrix algebra support
+      {:nx, "~> 0.7"}
     ]
   end
 
